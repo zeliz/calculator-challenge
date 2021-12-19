a23af7429bbdd9b95e53179bef9b5dc93cc03742
Zach Zeli <zelizach@gmail.com>
2021-12-19 10:43:35 -0500

    Initial setup

---
+++ CalculatorPkg/main/default/lwc/holder/holder.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class Holder extends LightningElement {
+    btnLabel = [
+        "7", "8", "9",
+        "4", "5", "6",
+        "1", "2", "3",
+    ];
+
+    btnLabel1 = 1;
+    btnLabel2 = 2;
+    btnLabel3 = 3;
+    btnLabel4 = 4;
+    btnLabel5 = 5;
+    btnLabel6 = 6;
+    btnLabel7 = 7;
+    btnLabel8 = 8;
+    btnLabel9 = 9;
+}
